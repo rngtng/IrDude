a9f0622a2ef57743bc2cd65f6fa59330d8954335
Tobias Bielohlawek <tobi@soundcloud.com>
2013-04-05 01:20:08 +0200

    add buttons, send correct code

--- src/com/rngtng/irdude/MainActivity.java
+++ src/com/rngtng/irdude/MainActivity.java
@@ -2,32 +2,70 @@ package com.rngtng.irdude;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
+	HashMap<Integer, String> irData;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        try {
-//            Object irdaService = this.getSystemService("irda");
-//            Class c = irdaService.getClass();
-//            Class p[] = {String.class};
-//            Method write = c.getMethod("write_irsend", p);
-//
-//            EditText te = (EditText)findViewById(R.id.sendSeq);
-//            write.invoke(irdaService, te.getText().toString());
-//    } catch (Exception e) {
-//            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
-//    }
+        irData = new HashMap<Integer, String>();
+        irData.put(R.id.buttonPower,   "0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0696 0152 0055 0015 0e23");
+        irData.put(R.id.buttonVolDown, "0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 0015 0015 003f 0015 003f 0015 0015 0015 0015 0015 0696 0152 0055 0015 0e23");
+        irData.put(R.id.buttonMute,    "0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 0015 0015 003f 0015 003f 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0696 0152 0055 0015 0e23");
+        irData.put(R.id.buttonVolUp,   "0000 006d 0022 0002 0152 00aa 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 0015 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 0015 0015 0015 0015 0015 0015 003f 0015 003f 0015 003f 0015 0015 0015 0015 0015 0696 0152 0055 0015 0e23");                    
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }   
+        
+    public void irSend(View view) {
+    	System.out.println(view.getId());
+    	
+    	String data = irData.get(view.getId());
+    	sendIr( hex2dec(data) );
+    }
+    
+    public String hex2dec(String irData) {
+    	List<String> list = new ArrayList<String>(Arrays.asList(irData.split(" ")));
+    	list.remove(0); // dummy
+    	int frequency = Integer.parseInt( list.remove(0), 16) ; // frequency    	
+    	list.remove(0); // seq1
+    	list.remove(0); // seq2    	
+    	
+    	for(int i=0; i<list.size(); i++){
+    		list.set(i, Integer.toString( Integer.parseInt(list.get(i), 16) ) );
+        }
+    	
+    	frequency = (int) (1000000 / (frequency * 0.241246));
+    	list.add(0, Integer.toString(frequency));
+    	
+    	irData = "";
+    	for (String s : list)
+    	{
+    		irData += s + ",";
+    	}    	
+    	return irData;
+   }
+    
+   public void sendIr(String irData) {    	
         Object irdaService = this.getSystemService("irda");
         Class c = irdaService.getClass();
         Class p[] = {String.class};
@@ -35,7 +73,7 @@ public class MainActivity extends Activity {
 		try {
 			write = c.getMethod("write_irsend", p);
 			try {
-				write.invoke(irdaService, "38400,173,171,24,62,24,61,24,62,24,17,24,17,24,18,24,17,24,170,24,62,24,61,24,62,24,17,24,19,23,17,24,17,24,17,24,62,24,61,25,61,24,62,24,18,23,17,24,17,25,17,24,17,24,17,24,18,24,17,24,62,24,61,24,62,24,61,24,1880");
+				write.invoke(irdaService,irData);
 			} catch (IllegalArgumentException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -50,17 +88,6 @@ public class MainActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
-
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
+ 
 }
